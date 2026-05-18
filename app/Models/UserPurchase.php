140d8b2ed39bb78dcdd1a3168d337757ee6495f6
Mohamedmagdy-9999 <mohamedmagdy111996@gmail.com>
2026-05-18 02:22:55 +0200

    order details

--- app/Models/UserPurchase.php
+++ app/Models/UserPurchase.php
@@ -11,7 +11,7 @@ class UserPurchase extends Model
      protected $guarded  = [];
 
      protected $appends = ['delivery_date'];
-     protected $hidden = ['address'];
+     
 
     public function user()
     {
